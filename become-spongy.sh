b9f76d801b76641ed82662f391f926f44d4d6260
Roberto Tyley <roberto.tyley@gmail.com>
2014-01-09 18:24:45 +0000

    flan?

--- become-spongy.sh
+++ become-spongy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash 
 
-# no further use for remaining crypto stuff
-
-rm -Rf crypto
-
 # Package rename org.bouncycastle to org.spongycastle
     
 find -name bouncycastle | xargs rename s/bouncycastle/spongycastle/
-find bc* -type f | xargs sed -i s/bouncycastle/spongycastle/g
+
+# find bc* -type f | xargs sed -i s/bouncycastle/spongycastle/g
 
 # BC to SC for provider name
     
@@ -15,12 +12,12 @@ find bc* -type f | xargs sed -i s/\"BC\"/\"SC\"/g
 
 # Rename 'bc' artifacts to 'sc'
     
-rename s/^bc/sc/ *
-find -name 'pom.xml' | xargs sed -i s/\>bc/\>sc/g
+# rename s/^bc/sc/ *
+# find -name 'pom.xml' | xargs sed -i s/\>bc/\>sc/g
 
 # Rename maven artifact 'names' to use Spongy rather than Bouncy
 
-find -name 'pom.xml' | xargs sed -i s/\>Bouncy/\>Spongy/g
+# find -name 'pom.xml' | xargs sed -i s/\>Bouncy/\>Spongy/g
 
     
 
